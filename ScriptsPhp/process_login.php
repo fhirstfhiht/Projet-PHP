ba73fd4a6d20a544fcbd6913a1289c3ce44ec43f
fhirstfhiht <issam.chouchane@etu.iut-tlse3.fr>
2025-01-17 14:20:56 +0100

    db

--- ScriptsPhp/process_login.php
+++ ScriptsPhp/process_login.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
-require_once 'config.php'; // Connexion à la base de données
+require_once '../SQL/db_connection.php'; // Inclure la fonction connectDB()
+
+// On crée l'instance PDO
+$pdo = connectDB();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Récupérer les données du formulaire
@@ -12,19 +15,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $query->bindParam(':username', $username, PDO::PARAM_STR);
     $query->execute();
     $user = $query->fetch(PDO::FETCH_ASSOC);
+
     // Si l'utilisateur existe et que le mot de passe est correct
     if ($user && password_verify($password, $user['Password'])) {
         // Connexion réussie
-        $_SESSION['user_id'] = $user['Id_Utilisateur'];  // Enregistrer l'ID de l'utilisateur dans la session
-        $_SESSION['username'] = $user['Login'];  // Enregistrer le login de l'utilisateur
+        $_SESSION['user_id'] = $user['Id_Utilisateur'];
+        $_SESSION['username'] = $user['Login'];
 
-        // Redirection vers la page d'accueil ou le tableau de bord       
-       header('Location: ../php/index.php');
+        // Redirection vers la page d'accueil ou le tableau de bord
+        header('Location: ../php/index.php');
         exit;
     } else {
-        // Identifiant ou mot de passe incorrect, rediriger avec un message d'erreur
+        // Identifiant ou mot de passe incorrect
         $_SESSION['error'] = 'Identifiant ou mot de passe incorrect.';
-        header('Location: ../php/Connexion.php');  // Rediriger vers la page de connexion
+        header('Location: ../php/Connexion.php');
         exit;
     }
 }
